5dde9052d02f6314ea507312718df4739c3c439f
mohrip <m25srt@gmail.com>
2025-10-02 09:22:06 +0300

    Add services inside category folder

---
+++ src/main/java/com/StackShop/project/Category/CategoryInterface.java
@@ -0,0 +1,10 @@
+package com.StackShop.project.Category;
+
+import java.util.List;
+
+// Interfaces instead class to support modularity and decoupling
+// This interface can be implemented by a class to provide actual service logic
+public interface CategoryInterface {
+    List<CategoryModel> getAllCategories();
+    Void createCategory(CategoryModel categoryModel);
+}
