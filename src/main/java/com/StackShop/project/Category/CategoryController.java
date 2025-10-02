2b8fa49f5f5c7b9d85197949e03e2152107cc3f6
mohrip <m25srt@gmail.com>
2025-10-02 09:53:03 +0300

    Add autogenerate unique ID

--- src/main/java/com/StackShop/project/Category/CategoryController.java
+++ src/main/java/com/StackShop/project/Category/CategoryController.java
@@ -1,10 +1,15 @@
 package com.StackShop.project.Category;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,17 +17,23 @@ import java.util.List;
 @RestController
 public class CategoryController {
 
-    private List<CategoryModel> categories = new ArrayList<>();
+    private CategoryInterface categoryInterface;
+
+    @Autowired
+    public CategoryController(CategoryInterface categoryInterface) {
+        this.categoryInterface = categoryInterface;
+    }
 
     @GetMapping("/api/public/categories")
     public List<CategoryModel> getAllCategories() {
-
-        return categories;
+    return categoryInterface.getAllCategories();
+       // return categories;
     }
 
     @PostMapping("/api/public/categories")
     public String creatCategory(@RequestBody  CategoryModel categoryModel) {
-        categories.add(categoryModel);
+        categoryInterface.createCategory(categoryModel);
+
         return "Categories added successfully!";
     }
 }
--- src/main/java/com/StackShop/project/Category/CategoryServiceImpl.java
+++ src/main/java/com/StackShop/project/Category/CategoryServiceImpl.java
@@ -1,4 +1,26 @@
 package com.StackShop.project.Category;
 
-public class CategoryServiceImpl {
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class CategoryServiceImpl implements CategoryInterface {
+    private List<CategoryModel> categories = new ArrayList<>();
+    private Long  nextId = 1L;
+
+    @Override
+    public List<CategoryModel> getAllCategories() {
+        return categories;
+    }
+
+    @Override
+    public Void createCategory(CategoryModel categoryModel) {
+        categoryModel.setCategoryId(nextId++);
+        categories.add(categoryModel);
+
+        return null;
+    }
 }
