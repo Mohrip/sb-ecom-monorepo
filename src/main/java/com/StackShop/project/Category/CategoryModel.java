7f0fca8aeb51b9774ba26a9ff1460c12dd6d5453
mohrip <m25srt@gmail.com>
2025-10-13 09:22:30 +0300

    refactor CategoryModel Class

--- src/main/java/com/StackShop/project/Category/CategoryModel.java
+++ src/main/java/com/StackShop/project/Category/CategoryModel.java
@@ -1,40 +1,17 @@
 package com.StackShop.project.Category;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "CATEGORY")
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
-    public CategoryModel() {
-    }
-
-    public CategoryModel(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public CategoryModel(String categoryName, Long categoryId) {
-        this.categoryName = categoryName;
-        this.categoryId = categoryId;
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
