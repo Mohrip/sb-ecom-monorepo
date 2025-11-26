8da63cd26192ddfb93b4c0f04e62eaa1561a3156
mohrip <m25srt@gmail.com>
2025-11-26 13:47:48 +0300

    (fix): CategoryService: clean the codebase

--- src/main/java/com/StackShop/project/Category/CategoryService.java
+++ src/main/java/com/StackShop/project/Category/CategoryService.java
@@ -6,7 +6,6 @@ import com.StackShop.project.Category.CategoryModel;
 // Interfaces instead class to support modularity and decoupling
 // This interface can be implemented by a class to provide actual service logic
 public interface CategoryService {
-   // long CATEGORY_ID_NOT_FOUND = -1L;
    // List<CategoryModel> getAllCategories();
     CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize);
    // Void createCategory(CategoryModel categoryModel);
