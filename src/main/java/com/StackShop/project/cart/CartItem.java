4089603169a29b87854d491ff43b7c6632007b25
mohrip <m25srt@gmail.com>
2025-12-22 20:00:49 +0300

    feat(Shopping Cart): add cart and CartItem models

--- src/main/java/com/StackShop/project/cart/CartItem.java
+++ src/main/java/com/StackShop/project/cart/CartItem.java
@@ -1,4 +1,28 @@
 package com.StackShop.project.cart;
 
+import com.StackShop.project.product.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cart_items")
+@NoArgsConstructor
+@AllArgsConstructor
 public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+    private Double discount;
+    private Double productPrice;
 }
